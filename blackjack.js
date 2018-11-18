e72c1588d5bb7b9d7941b36446b00c863e786cc4
Vlad6575 <ayoureadyforameracle@yandex.ru>
2018-11-18 14:34:46 +0300

    Created Deck class

--- blackjack.js
+++ blackjack.js
@@ -62,4 +62,12 @@ var BlackjackJS = (function() {
         `;
     }
 
+    /*************************
+        Deck - Singleton class
+    *************************/
+    var Deck = new function(){
+        this.ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+        this.suits = ['hearts', 'spades', 'diamonds','clubs'];
+        this.deck;
+    }
 }
